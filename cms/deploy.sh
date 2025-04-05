acfc12c832d5f9474c8fb69cb499f60b9188330d
EC2 Default User <ec2-user@ip-172-31-75-155.ec2.internal>
2025-04-05 20:54:29 +0000

    modified deploy script

--- cms/deploy.sh
+++ cms/deploy.sh
@@ -10,11 +10,15 @@ NODE_ENV=production
 npm run build
 
 # Copy the built files to AWS S3 bucket
+echo "Copying build to AWS S3..."
 source ../cms/.env
 S3_BUCKET="rwandanurture.org"
-aws s3 sync ./out s3://$S3_BUCKET --delete
+aws s3 sync ./out s3://$S3_BUCKET --delete > /dev/null
 
 # Invalidate cache to see new frontend
+echo "Clearing AWS distribution cache..."
 aws cloudfront create-invalidation \
     --distribution-id $DISTRIBUTION \
-    --paths "/*"
+    --paths "/*" > /dev/null
+
+echo "Done!"
